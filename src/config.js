7e3c9df0928c34dd8a2fedd309c9ad39017b3f91
ravibabuvadde <79750913+ravibabuvadde@users.noreply.github.com>
2022-09-05 19:58:48 +0530

    .env removed from .gitignore

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 module.exports = {
-  port: process.env.PORT || 3000,
-  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017",
-  CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  JWT_SECRET: process.env.JWT_SECRET,
+  port: process.env.PORT || 80,
+  mongoUrl: process.env.MONGO_URL|| "mongodb+srv://admin:team_escr@meethome.lqy171y.mongodb.net/?retryWrites=true&w=majority",
+  CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "985697523517-dkp2f6qrf6bo51mi8416mj3e41jcvpm9.apps.googleusercontent.com",
+  JWT_SECRET: process.env.JWT_SECRET ||"team_escr"
 };
