12ca68c7ae6639c319428e58d1b6da1f5d44c8bc
ravibabuvadde <79750913+ravibabuvadde@users.noreply.github.com>
2023-01-01 18:05:20 +0530

    abstracting the config to a separate file

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 module.exports = {
   port: process.env.PORT || 80,
-  mongoUrl: process.env.MONGO_URL|| "mongodb+srv://admin:team_escr@meethome.lqy171y.mongodb.net/?retryWrites=true&w=majority",
-  CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "985697523517-dkp2f6qrf6bo51mi8416mj3e41jcvpm9.apps.googleusercontent.com",
-  JWT_SECRET: process.env.JWT_SECRET ||"team_escr"
+  mongoUrl: process.env.MONGO_URL,
+  CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  JWT_SECRET: process.env.JWT_SECRET
 };
